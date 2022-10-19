e66fb0a59ad7d9cf724a761264ca1a9162c4add9
clchen-arcadia <clchen.arcadia@gmail.com>
2022-10-18 20:28:27 -0400

    Finish Part 1 outer_space and got code reviewed

--- outer_space.sql
+++ outer_space.sql
@@ -1,31 +1,31 @@
 CREATE TABLE stars (
     name VARCHAR(50) PRIMARY KEY,
-    temp INT NOT NULL);
+    temp_kelvin INT NOT NULL);
 
 CREATE TABLE planets (
     name VARCHAR(50) PRIMARY KEY,
-    orbital_period FLOAT NOT NULL,
-    star_id VARCHAR(50) NOT NULL REFERENCES stars);
+    orbital_period_yrs FLOAT NOT NULL,
+    star_name VARCHAR(50) NOT NULL REFERENCES stars);
 
 
 CREATE TABLE moons (
     name VARCHAR(50) PRIMARY KEY,
-    planet_id VARCHAR(50) REFERENCES planets);
+    planet_name VARCHAR(50) REFERENCES planets);
 
 
-INSERT INTO stars (name, temp) VALUES
+INSERT INTO stars (name, temp_kelvin) VALUES
 ('The Sun', 5800),
 ('Proxima Centauri', 3042),
 ('Gliese 876', 3192);
 
-INSERT INTO planets (name, orbital_period, star_id) VALUES
+INSERT INTO planets (name, orbital_period_yrs, star_name) VALUES
 ('Earth', 1.00, 'The Sun'),
 ('Mars', 1.882, 'The Sun'),
 ('Venus', 0.62, 'The Sun'),
 ('Proxima Centauri b', 0.03, 'Proxima Centauri'),
 ('Gliese 876 b', 0.236, 'Gliese 876');
 
-INSERT INTO moons (name, planet_id) VALUES
+INSERT INTO moons (name, planet_name) VALUES
 ('The Moon', 'Earth'),
 ('Phobos', 'Mars'),
 ('Deimos', 'Mars');
--- queries.sql
+++ queries.sql
@@ -1,21 +1,24 @@
 
--- SELECT planets.name AS planet, stars.name AS star, COUNT(*) AS moon_count
---     FROM planets
---         LEFT JOIN stars ON planets.star_id = stars.name
---         LEFT JOIN moons ON planets.name = moons.planet_id
---     GROUP BY planets.name
+SELECT
+    planets.name AS planet,
+    stars.name AS star,
+    SUM(CASE WHEN moons.name IS NULL THEN 0 ELSE 1 END)
+FROM planets
+    LEFT JOIN stars ON planets.star_id = stars.name
+    LEFT JOIN moons ON moons.planet_id = planets.name
+GROUP BY planets.name, stars.name
+ORDER BY planets.name;
 
 
+SELECT
+    planets.name AS planet,
+    stars.name AS star,
+    COUNT(moons.name)
+FROM
+    planets
+    LEFT JOIN stars ON planets.star_id = stars.name
+    LEFT JOIN moons ON moons.planet_id = planets.name
+GROUP BY planets.name, stars.name
+ORDER BY planets.name;
 
-SELECT planets.name AS planet, stars.name AS star, COUNT(*) as count_star
-    FROM planets
-        LEFT JOIN stars ON planets.star_id = stars.name
-        LEFT JOIN moons ON moons.planet_id = planets.name
-    GROUP BY planets.name, stars.name
-    ORDER BY planets.name;
-
-
-
--- , CASE WHEN moons.name IS NULL THEN 1 ELSE 0 END AS test
-
--- , stars.name
+--COUNT of expression counts non null things. COUNT * is everything
